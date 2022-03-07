4161105997a4d266f62531c74edcc1cf1d2f78f7
ryan <ryanrogo064@gmail.com>
2022-03-06 19:15:02 -0500

    finish #14

--- problem_14.py
+++ problem_14.py
@@ -1,7 +1,7 @@
 import time
 
 start = time.time()
-fullList = []
+curr_big, big_num = 0, 0
 
 def get_num(number):
     count = 1
@@ -13,11 +13,11 @@ def get_num(number):
         else:
             number1 = (3*number1) + 1
             count += 1
-    return [count, number]
+    return count, number
 
 for i in range(1, 1000000):
-	fullList.append(get_num(i))
+	count, number = get_num(i)
+	if count > curr_big:
+		curr_big, big_num = count, number
 
-sortedList = sorted(fullList, reverse = True)
-print(*sortedList[:1])
-print("Time: " + str(int(time.time()-start)) + " seconds")
+print("The number is " + str(big_num) + ". Took: " + str(int(time.time()-start)) + " seconds")
