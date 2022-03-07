ab363b3798ec89bb2f1921c6d1a6a258ff7f4a59
ryan <ryanrogo064@gmail.com>
2022-03-07 16:50:25 -0500

    Update problem_17.py

--- problem_17.py
+++ problem_17.py
@@ -8,21 +8,19 @@ dic[20] = dic[30] = dic[11] = dic[12] = dic[80] = dic[90] = 6
 dic[15] = dic[16] = dic[70] = 7
 dic[18] = dic[19] = dic[14] = dic[13] = 8
 dic[17] = 9
+dic[1000] = 11
 
 for i in range(21,100):
 	tens = int(i/10)*10
 	ones = i - tens
-	dic[i]  = dic[tens]+dic[ones]
+	dic[i] = dic[tens]+dic[ones]
 
 for i in range(100,1000):
 	hundreds = int(i/100)
 	tens_ones = i - hundreds*100
-
 	if tens_ones == 0:
 		dic[i] = dic[hundreds] + 7
 	else:
-		dic[i] = dic[hundreds] +10+dic[tens_ones]
-
-dic[1000] = 11
+		dic[i] = dic[hundreds] + 10 + dic[tens_ones]
 
 print(sum(dic.values()))
